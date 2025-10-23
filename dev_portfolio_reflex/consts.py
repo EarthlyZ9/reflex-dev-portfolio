d74ce4936025bff97b6ed19d1c48ee47b539607b
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-10-24 07:03:46 +0900

    chore: cv url 변경

--- dev_portfolio_reflex/consts.py
+++ dev_portfolio_reflex/consts.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 GITHUB_URL = "https://github.com/EarthlyZ9"
 BLOG_URL = "https://earthlyz9.notion.site/"
-CV_URL = "https://www.rallit.com/resumes/11292@linda2927/%EC%9D%B4%EC%A7%80%EC%88%98"
+CV_URL = "https://www.notion.so/earthlyz9/Backend-Engineer-28812f03c2df805284f5f83d76b191cb"
 NOTION_API_KEY = os.getenv("NOTION_API_KEY")
